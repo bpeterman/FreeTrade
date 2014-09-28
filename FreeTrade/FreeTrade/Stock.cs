ec71c3ab198f8777fb145911fea3f5628348494a
Blake Peterman <peterman.blake@gmail.com>
2014-09-28 17:39:43 -0400

    Created a Stock class
    Created a getLatestValue function and a getFromAPI function to make
    future methods easier.

---
+++ FreeTrade/FreeTrade/Stock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace FreeTrade
+{
+    class Stock
+    {
+        public double getLatestValue(string symbol)
+        {
+            return Convert.ToDouble(getFromAPI(symbol, "l1")[0]);
+        }
+        private string[] getFromAPI(string symbol, string args)
+        {
+            string[] results = null;
+            try
+            {
+                string temp = null;
+                string yahooURL = @"http://download.finance.yahoo.com/d/quotes.csv?s=" +
+                                      symbol + "&f=" + args;
+                // Initialize a new WebRequest.
+                HttpWebRequest webreq = (HttpWebRequest)WebRequest.Create(yahooURL);
+                // Get the response from the Internet resource.
+                HttpWebResponse webresp = (HttpWebResponse)webreq.GetResponse();
+                // Read the body of the response from the server.
+                StreamReader strm =
+                  new StreamReader(webresp.GetResponseStream(), Encoding.ASCII);
+                temp = strm.ReadToEnd();
+                results = temp.Split(new Char[] { ',' });
+                strm.Close();
+            }
+            catch
+            {
+                //some exception
+            }
+            return results;
+        }
+    }
+}

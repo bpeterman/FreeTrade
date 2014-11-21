1326d6c6d4a63ecb84e1b0919500fbbac4613412
Blake Peterman <peterman.blake@gmail.com>
2014-11-21 00:41:22 -0500

    Added the new user form back

--- FreeTradeWindowsForms/FreeTradeWindowsForms/NewUser.Designer.cs
+++ FreeTradeWindowsForms/FreeTradeWindowsForms/NewUser.Designer.cs
@@ -118,7 +118,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 207);
+            this.ClientSize = new System.Drawing.Size(250, 207);
             this.Controls.Add(this.startCashBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.addUserButton);

b13f6274501f55b5779210b46694dfcfa80429f9
bcamadin <bcamadin@spsu.edu>
2014-11-25 02:38:35 +0000

    this is a test

--- FreeTradeWindowsForms/FreeTradeWindowsForms/Program.cs
+++ FreeTradeWindowsForms/FreeTradeWindowsForms/Program.cs
@@ -15,7 +15,7 @@ namespace FreeTradeWindowsForms
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false); 
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
     }
